3140b2ae9e9eed5ffde3d1e692824a83b22d65a6
Ahmad Luthfi Nasrulloh <luthfiahmad36@gmail.com>
2021-09-02 09:46:18 +0700

    row dan column

--- lib/main.dart
+++ lib/main.dart
@@ -11,15 +11,46 @@ class HalHallo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      backgroundColor: Colors.yellow[200],
-      appBar: new AppBar(
-        backgroundColor: Colors.red[400],
-        leading: new Icon(Icons.home),
-        title: new Center(child: new Text("Home"),),
-        actions: <Widget>[
-          new Icon(Icons.search)
-        ],
-      ),
-    );
+        backgroundColor: Colors.yellow[200],
+        appBar: new AppBar(
+          backgroundColor: Colors.red[400],
+          leading: new Icon(Icons.home),
+          title: new Center(
+            child: new Text("Home"),
+          ),
+          actions: <Widget>[new Icon(Icons.search)],
+        ),
+        body: new Container(
+            child: new Column(
+          children: <Widget>[
+            new Icon(
+              Icons.local_pizza,
+              size: 70.0,
+              color: Colors.red,
+            ),
+            new Row(children: <Widget>[
+              new Icon(
+                Icons.local_pizza,
+                size: 70.0,
+                color: Colors.red,
+              ),
+              new Icon(
+                Icons.local_pizza,
+                size: 70.0,
+                color: Colors.red,
+              ),
+              new Icon(
+                Icons.local_pizza,
+                size: 70.0,
+                color: Colors.red,
+              ),
+            ]),
+            new Icon(
+              Icons.cake,
+              size: 70.0,
+              color: Colors.red,
+            )
+          ],
+        )));
   }
 }
