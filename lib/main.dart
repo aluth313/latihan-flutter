ee3aea9cb077c40b1bf81d806fadd6030b759bbb
Ahmad Luthfi Nasrulloh <luthfiahmad36@gmail.com>
2021-09-02 14:49:39 +0700

    sidebar (drawer) & user profile

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './detail.dart';
 
 void main() {
   runApp(new MaterialApp(
@@ -13,82 +14,78 @@ class HalHallo extends StatefulWidget {
 }
 
 class _HalHalloState extends State<HalHallo> {
-  String teks = "";
-  final GlobalKey<ScaffoldState> _scaffoldState = new GlobalKey<ScaffoldState>();
+  String gambar1 =
+      "https://blue.kumparan.com/image/upload/fl_progressive,fl_lossy,c_fill,q_auto:best,w_640/v1619493558/u3wutzogf853ksr2lncw_ujx6f6.jpg";
+  String gambar2 =
+      "https://cdn-2.tstatic.net/batam/foto/bank/images/poster-film-avengers-infinity-war_20180423_071011.jpg";
+  String backup;
+  String nama1 = "Ujang";
+  String nama2 = "Avengers";
+  String backupnama;
 
-  TextEditingController controller = new TextEditingController();
-  TextEditingController controllerAlert = new TextEditingController();
-  TextEditingController controllerSnackbar = new TextEditingController();
+  void gantiUser() {
+    this.setState(() {
+      backup = gambar1;
+      gambar1 = gambar2;
+      gambar2 = backup;
 
-  void _alertdialog(String str) {
-    if (str.isEmpty) return;
-    AlertDialog alertDialog = new AlertDialog(
-      content: new Text(
-        str,
-        style: new TextStyle(fontSize: 20.0),
-      ),
-      actions: <Widget>[
-        new RaisedButton(color: Colors.purple, child: new Text("OK"), onPressed: (){
-          Navigator.pop(context);
-        })
-      ],
-    );
-
-    showDialog(context: context, child: alertDialog);
-  }
-  
-  void _snackbar(String str) {
-    if (str.isEmpty) return;
-    _scaffoldState.currentState.showSnackBar(new SnackBar(
-      content: new Text(str, style: new TextStyle(fontSize: 20.0),),
-      duration: new Duration(seconds: 3),
-      ));
+      backupnama = nama1;
+      nama1 = nama2;
+      nama2 = backupnama;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      key: _scaffoldState,
       appBar: new AppBar(
-        title: new Text("INPUT TEXT, ALERT DIALOG & SNACKBAR"),
-        backgroundColor: Colors.purple,
+        title: new Text("Demo Sidebar (DRAWER)"),
+        backgroundColor: Colors.red,
       ),
-      body: new Container(
-        child: new Column(
+      drawer: new Drawer(
+        child: new ListView(
           children: <Widget>[
-            new TextField(
-              controller: controller,
-              decoration: new InputDecoration(hintText: "Tulis disini"),
-              onSubmitted: (String str) {
-                setState(() {
-                  teks = str + '\n' + teks;
-                  controller.text = "";
-                });
-              },
+            new UserAccountsDrawerHeader(
+              accountName: new Text(nama1),
+              accountEmail: new Text("ujang@gmail.com"),
+              currentAccountPicture: new GestureDetector(
+                onTap: () {
+                  Navigator.of(context).pop();
+                  Navigator.of(context).push(new MaterialPageRoute(
+                      builder: (BuildContext context) => new Detail(
+                            nama: nama1,
+                            gambar: gambar1,
+                          )));
+                },
+                child: new CircleAvatar(
+                  backgroundImage: new NetworkImage(gambar1),
+                ),
+              ),
+              decoration: new BoxDecoration(
+                  image: new DecorationImage(
+                      image: new NetworkImage(
+                          "https://cdn0-production-images-kly.akamaized.net/M214oOJllSwlY3KqwGxuJ2G6qw4=/640x360/smart/filters:quality(75):strip_icc():format(jpeg)/kly-media-production/medias/3343563/original/001361400_1610083119-neon-light-arrow-direction-perspective_1017-22033.jpg"),
+                      fit: BoxFit.cover)),
+              otherAccountsPictures: <Widget>[
+                new GestureDetector(
+                    onTap: () => gantiUser(),
+                    child: new CircleAvatar(
+                      backgroundImage: new NetworkImage(gambar2),
+                    )),
+              ],
             ),
-            new Text(
-              teks,
-              style: new TextStyle(fontSize: 20.0),
+            new ListTile(
+              title: new Text("Setting"),
+              trailing: new Icon(Icons.settings),
             ),
-            new TextField(
-              controller: controllerAlert,
-              decoration: new InputDecoration(hintText: "ini untuk alert"),
-              onSubmitted: (String str) {
-                _alertdialog(str);
-                controllerAlert.text="";
-              },
-            ),
-            new TextField(
-              controller: controllerSnackbar,
-              decoration: new InputDecoration(hintText: "tulis untuk snackbar"),
-              onSubmitted: (String str) {
-                _snackbar(str);
-                controllerSnackbar.text="";
-              },
+            new ListTile(
+              title: new Text("Close"),
+              trailing: new Icon(Icons.close),
             ),
           ],
         ),
       ),
+      body: new Container(),
     );
   }
 }
