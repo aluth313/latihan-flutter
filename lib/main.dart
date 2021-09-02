80e17bec6560422a41184321413d9c805b91a93a
Ahmad Luthfi Nasrulloh <luthfiahmad36@gmail.com>
2021-09-02 10:49:10 +0700

    tap bar & bottom bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,52 +1,92 @@
 import 'package:flutter/material.dart';
+import './hal_headset.dart' as headset;
+import './hal_komputer.dart' as komputer;
+import './hal_radio.dart' as radio;
+import './hal_smartphone.dart' as smartphone;
 
 void main() {
   runApp(new MaterialApp(
-    title: "Navigasi",
+    title: "Tap Bar",
     home: new HalHallo(),
-    routes: <String, WidgetBuilder>{
-      '/HalHallo' : (BuildContext context) => new HalHallo(),
-      '/HalDua' : (BuildContext context) => new HalDua(),
-    },
   ));
 }
 
-class HalHallo extends StatelessWidget {
+class HalHallo extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-        appBar: new AppBar(
-          title: new Text("Music"),
-        ),
-        body: new Center(
-          child: new IconButton(
-              icon: new Icon(
-                Icons.headset,
-                size: 50.0,
-              ),
-              onPressed: (){
-                Navigator.pushNamed(context, '/HalDua');
-              }),
-        ));
-  }
+  _HalHalloState createState() => _HalHalloState();
 }
 
-class HalDua extends StatelessWidget {
+class _HalHalloState extends State<HalHallo>
+    with SingleTickerProviderStateMixin {
+  TabController controller;
+
+  @override
+  void initState() {
+    controller = new TabController(length: 4, vsync: this);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-        appBar: new AppBar(
-          title: new Text("Speaker"),
+      appBar: new AppBar(
+        backgroundColor: Colors.amber,
+        title: new Text("Daftar Elektronik"),
+        bottom: new TabBar(
+          controller: controller,
+          tabs: <Widget>[
+            new Tab(
+              icon: new Icon(Icons.computer),
+              text: "Komputer",
+            ),
+            new Tab(
+              icon: new Icon(Icons.headset),
+              text: "Headset",
+            ),
+            new Tab(
+              icon: new Icon(Icons.radio),
+              text: "Radio",
+            ),
+            new Tab(
+              icon: new Icon(Icons.smartphone),
+              text: "Smartphone",
+            ),
+          ],
         ),
-        body: new Center(
-          child: new IconButton(
-              icon: new Icon(
-                Icons.speaker,
-                size: 50.0,
-              ),
-              onPressed: (){
-                Navigator.pushNamed(context, '/HalHallo');
-              }),
-        ));
+      ),
+      body: new TabBarView(controller: controller, children: <Widget>[
+        new komputer.Komputer(),
+        new headset.Headset(),
+        new radio.Radio(),
+        new smartphone.Smartphone(),
+      ]),
+      
+      bottomNavigationBar: new Material(
+        color: Colors.amber,
+        child: new TabBar(
+          controller: controller,
+          tabs: <Widget>[
+            new Tab(
+              icon: new Icon(Icons.computer),
+            ),
+            new Tab(
+              icon: new Icon(Icons.headset),
+            ),
+            new Tab(
+              icon: new Icon(Icons.radio),
+            ),
+            new Tab(
+              icon: new Icon(Icons.smartphone),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
