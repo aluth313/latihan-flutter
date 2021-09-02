7fac544638a3b424987761e297ffec7db697af9c
Ahmad Luthfi Nasrulloh <luthfiahmad36@gmail.com>
2021-09-02 11:27:26 +0700

    List View

--- lib/main.dart
+++ lib/main.dart
@@ -1,89 +1,79 @@
 import 'package:flutter/material.dart';
-import './hal_headset.dart' as headset;
-import './hal_komputer.dart' as komputer;
-import './hal_radio.dart' as radio;
-import './hal_smartphone.dart' as smartphone;
 
 void main() {
   runApp(new MaterialApp(
-    title: "Tap Bar",
+    title: "List View",
     home: new HalHallo(),
   ));
 }
 
-class HalHallo extends StatefulWidget {
-  @override
-  _HalHalloState createState() => _HalHalloState();
-}
-
-class _HalHalloState extends State<HalHallo>
-    with SingleTickerProviderStateMixin {
-  TabController controller;
-
-  @override
-  void initState() {
-    controller = new TabController(length: 4, vsync: this);
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    controller.dispose();
-    super.dispose();
-  }
-
+class HalHallo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
-        backgroundColor: Colors.amber,
-        title: new Text("Daftar Elektronik"),
-        bottom: new TabBar(
-          controller: controller,
-          tabs: <Widget>[
-            new Tab(
-              icon: new Icon(Icons.computer),
-              text: "Komputer",
-            ),
-            new Tab(
-              icon: new Icon(Icons.headset),
-              text: "Headset",
-            ),
-            new Tab(
-              icon: new Icon(Icons.radio),
-              text: "Radio",
-            ),
-            new Tab(
-              icon: new Icon(Icons.smartphone),
-              text: "Smartphone",
-            ),
-          ],
-        ),
+        backgroundColor: Colors.amber[900],
+        title: new Text("Listview"),
       ),
-      body: new TabBarView(controller: controller, children: <Widget>[
-        new komputer.Komputer(),
-        new headset.Headset(),
-        new radio.Radio(),
-        new smartphone.Smartphone(),
-      ]),
-      
-      bottomNavigationBar: new Material(
-        color: Colors.amber,
-        child: new TabBar(
-          controller: controller,
-          tabs: <Widget>[
-            new Tab(
-              icon: new Icon(Icons.computer),
+      body: new ListView(
+        children: <Widget>[
+          new ListAlat(
+            gambar:
+                "https://w7.pngwing.com/pngs/346/79/png-transparent-internet-radio-computer-icons-radio-electronics-fm-broadcasting-radio-station-thumbnail.png",
+            judul: "RADIO",
+          ),
+          new ListAlat(
+            gambar:
+                "https://png.pngtree.com/element_our/20190602/ourmid/pngtree-hand-drawn-smartphone-illustration-image_1401996.jpg",
+            judul: "SMARTPHONE",
+          ),
+          new ListAlat(
+            gambar:
+                "https://png.pngtree.com/element_our/20190602/ourmid/pngtree-hand-drawn-smartphone-illustration-image_1401996.jpg",
+            judul: "SMARTPHONE",
+          ),
+          new ListAlat(
+            gambar:
+                "https://png.pngtree.com/element_our/20190602/ourmid/pngtree-hand-drawn-smartphone-illustration-image_1401996.jpg",
+            judul: "SMARTPHONE",
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ListAlat extends StatelessWidget {
+  ListAlat({this.gambar, this.judul});
+
+  final String gambar;
+  final String judul;
+
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      padding: new EdgeInsets.all(20.0),
+      child: new Center(
+        child: new Row(
+          children: <Widget>[
+            new Image(
+              image: new NetworkImage(gambar),
+              width: 200.0,
             ),
-            new Tab(
-              icon: new Icon(Icons.headset),
-            ),
-            new Tab(
-              icon: new Icon(Icons.radio),
-            ),
-            new Tab(
-              icon: new Icon(Icons.smartphone),
-            ),
+            new Container(
+              padding: new EdgeInsets.all(20.0),
+              child: new Center(
+                child: new Column(
+                  children: <Widget>[
+                    new Text(
+                      judul,
+                      style: new TextStyle(fontSize: 20.0),
+                    ),
+                    new Text("ini adalah deskripsi", style: new TextStyle(fontSize: 15.0, color: Colors.grey),)
+                  ],
+                ),
+              ),
+            )
           ],
         ),
       ),
