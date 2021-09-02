0e5ea9917fa72ecce1e80babb6d3bf6d440e736b
Ahmad Luthfi Nasrulloh <luthfiahmad36@gmail.com>
2021-09-02 13:21:12 +0700

    retrieve data from JSON (API)

--- lib/main.dart
+++ lib/main.dart
@@ -1,72 +1,65 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'dart:convert';
 
 void main() {
   runApp(new MaterialApp(
     title: "List View",
-    home: new HalHallo(data: new List<String>.generate(300, (index) => "ini data ke $index"),),
+    home: new HalHallo(),
   ));
 }
 
-class HalHallo extends StatelessWidget {
-  final List<String> data;
-  HalHallo({this.data});
+class HalHallo extends StatefulWidget {
+  @override
+  _HalHalloState createState() => _HalHalloState();
+}
+
+class _HalHalloState extends State<HalHallo> {
+  List dataJSON;
+  Future<String> ambilData() async {
+    http.Response hasil = await http.get(
+        Uri.encodeFull("https://jsonplaceholder.typicode.com/posts"),
+        headers: {"Accept": "application/json"});
+
+    this.setState(() {
+      dataJSON = json.decode(hasil.body);
+    });
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    this.ambilData();
+  }
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
-        backgroundColor: Colors.amber[900],
-        title: new Text("Listview"),
+        title: new Text("List Data JSON"),
       ),
-      body: new Container(
-        child: new ListView.builder(
-          itemCount: data.length,
-          itemBuilder: (context, index){
-            return new ListTile(
-              leading: new Icon(Icons.widgets),
-              title: new Text("${data[index]}"),
-            );
-          },
-        ),
-      )
-    );
-  }
-}
-
-class ListAlat extends StatelessWidget {
-  ListAlat({this.gambar, this.judul});
-
-  final String gambar;
-  final String judul;
-
-  @override
-  Widget build(BuildContext context) {
-    return new Container(
-      padding: new EdgeInsets.all(20.0),
-      child: new Center(
-        child: new Row(
-          children: <Widget>[
-            new Image(
-              image: new NetworkImage(gambar),
-              width: 200.0,
-            ),
-            new Container(
-              padding: new EdgeInsets.all(20.0),
-              child: new Center(
-                child: new Column(
-                  children: <Widget>[
-                    new Text(
-                      judul,
-                      style: new TextStyle(fontSize: 20.0),
-                    ),
-                    new Text("ini adalah deskripsi", style: new TextStyle(fontSize: 15.0, color: Colors.grey),)
-                  ],
-                ),
-              ),
-            )
-          ],
-        ),
-      ),
+      body: new ListView.builder(
+          itemCount: dataJSON == null ? 0 : dataJSON.length,
+          itemBuilder: (context, i) {
+            return new Container(
+                padding: new EdgeInsets.all(20.0),
+                child: new Card(
+                    child: new Container(
+                  padding: new EdgeInsets.all(20.0),
+                  child: new Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      new Text(
+                        dataJSON[i]['title'],
+                        style:
+                            new TextStyle(fontSize: 20.0, color: Colors.blue),
+                      ),
+                      new Text(dataJSON[i]['body'])
+                    ],
+                  ),
+                )));
+          }),
     );
   }
 }
