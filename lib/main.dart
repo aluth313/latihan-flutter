c154a1d13a6efe41ea342313c47605dfc0db611a
Ahmad Luthfi Nasrulloh <luthfiahmad36@gmail.com>
2021-09-06 10:57:22 +0700

    datatables with retrieve API

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,11 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
-import 'package:flutter_coba/drawerku.dart';
-import 'package:flutter_coba/home.dart';
-import 'package:flutter_coba/profile.dart';
-import 'package:flutter_coba/setting.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(new MaterialApp(
-    title: "Fragment",
+    title: "Data Table",
     home: new HalHallo(),
   ));
 }
@@ -17,33 +16,23 @@ class HalHallo extends StatefulWidget {
 }
 
 class _HalHalloState extends State<HalHallo> {
-  String gambar1 =
-      "https://blue.kumparan.com/image/upload/fl_progressive,fl_lossy,c_fill,q_auto:best,w_640/v1619493558/u3wutzogf853ksr2lncw_ujx6f6.jpg";
-  String gambar2 =
-      "https://cdn-2.tstatic.net/batam/foto/bank/images/poster-film-avengers-infinity-war_20180423_071011.jpg";
-  String backup;
-  String nama1 = "Ujang";
-  String nama2 = "Avengers";
-  String backupnama;
+  List dataJson;
+  Future<String> ambilData() async {
+    http.Response hasil = await http.get(
+      Uri.encodeFull("https://jsonplaceholder.typicode.com/posts"),
+      headers: {"Accept": "application/json"}
+    );
 
-  void gantiUser() {
     this.setState(() {
-      backup = gambar1;
-      gambar1 = gambar2;
-      gambar2 = backup;
-
-      backupnama = nama1;
-      nama1 = nama2;
-      nama2 = backupnama;
+      dataJson = json.decode(hasil.body);
     });
   }
 
-  int index=0;
-  List<Widget> list = [
-    Home(),
-    Profile(),
-    Setting()
-  ];
+  @override
+  void initState() {
+    // TODO: implement initState
+    this.ambilData();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -51,90 +40,59 @@ class _HalHalloState extends State<HalHallo> {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Navigation Drawer"),
+          title: Text("Data Table"),
         ),
-        body: list[index],
-        drawer: Drawerku(onTap: (ctx,i){
-          setState(() {
-            index=i;
-            Navigator.pop(ctx);
-          });
-        }),
-      ),
-    );
-  }
-}
-
-class MyDrawer extends StatelessWidget {
-  final Function onTap;
-  MyDrawer({this.onTap});
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      width: MediaQuery.of(context).size.width*0.8,
-      child: Drawer(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
+        body: ListView(
           children: [
-            DrawerHeader(
-              decoration: BoxDecoration(color: Colors.blue),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Container(
-                    width: 60,
-                    height: 60,
-                    child: CircleAvatar(
-                      backgroundImage: NetworkImage("https://cdn-2.tstatic.net/batam/foto/bank/images/poster-film-avengers-infinity-war_20180423_071011.jpg"),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 15,
-                  ),
-                  Text(
-                    "Ahmad Luthfi",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 16,
-                      fontWeight: FontWeight.w600
-                    ),
-                  ),
-                  SizedBox(
-                    height: 3,
-                  ),
-                  Text(
-                    "luthfiahmad36@gmail.com",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 12
-                    ),
-                  )
-                ],
+            Center(
+              child: Text(
+                "List",
+                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
               ),
             ),
-            ListTile(
-              leading: Icon(Icons.home),
-              title: Text("Home"),
-              onTap: ()=> onTap(context,0),
-            ),
-            ListTile(
-              leading: Icon(Icons.person),
-              title: Text("Profile"),
-              onTap: ()=> onTap(context,1),
-            ),
-            ListTile(
-              leading: Icon(Icons.settings),
-              title: Text("Settings"),
-              onTap: ()=> onTap(context,2),
-            ),
-            Divider(height: 1,),
-            ListTile(
-              leading: Icon(Icons.exit_to_app),
-              title: Text("Logout"),
-              onTap: ()=> onTap(context,0),
-            ),
+            DataTable(columns: [
+              DataColumn(
+                  label: Text("ID",
+                      style: TextStyle(
+                          fontSize: 18, fontWeight: FontWeight.bold))),
+              DataColumn(
+                  label: Text("Title",
+                      style: TextStyle(
+                          fontSize: 18, fontWeight: FontWeight.bold))),
+              DataColumn(
+                  label: Text("Body",
+                      style: TextStyle(
+                          fontSize: 18, fontWeight: FontWeight.bold))),
+              DataColumn(
+                  label: Text("Nyoba",
+                      style: TextStyle(
+                          fontSize: 18, fontWeight: FontWeight.bold))),
+            ], rows: List.generate(dataJson.length, (index) {
+              return DataRow(cells: [
+                DataCell(Text(dataJson[index]['id'].toString())),
+                DataCell(Text(dataJson[index]['title'].toString())),
+                DataCell(Text(dataJson[index]['body'].toString())),
+                DataCell(Text(dataJson[index]['body'].toString())),
+              ]);
+            })
+            // [
+            //   DataRow(cells: [
+            //     DataCell(Text("1")),
+            //     DataCell(Text("Dadang")),
+            //     DataCell(Text("Dosen")),
+            //   ]),
+            //   DataRow(cells: [
+            //     DataCell(Text("2")),
+            //     DataCell(Text("Ujang")),
+            //     DataCell(Text("Staf")),
+            //   ]),
+            //   DataRow(cells: [
+            //     DataCell(Text("3")),
+            //     DataCell(Text("Jono")),
+            //     DataCell(Text("OB")),
+            //   ]),
+            // ]
+            )
           ],
         ),
       ),
